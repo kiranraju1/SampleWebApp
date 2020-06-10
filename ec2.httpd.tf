65250885c7670dbfe46210f5d847df0445350e20
kiranraju1 <kiranraju.devops@gmail.com>
2020-06-10 22:15:11 +0530

    Update ec2.httpd.tf

--- ec2.httpd.tf
+++ ec2.httpd.tf
@@ -7,8 +7,7 @@ provider "aws" {
 resource "aws_instance" "TF-Demo" {
   ami           = "ami-09d95fab7fff3776c"
   instance_type = "t2.micro"
-  user_data     = 
-                  #!/bin/bash
+  user_data     = #!/bin/bash
                   sudo su
                   yum -y install httpd
                   echo "<p> TF-Demo! </p>" >> /var/www/html/index.html
