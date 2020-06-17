4e1c5533bea60bb12cf08f4b673be4bc7051e884
kiranraju1 <kiranraju.devops@gmail.com>
2020-06-17 21:41:19 +0530

    Update ec2.httpd.tf

--- ec2.httpd.tf
+++ ec2.httpd.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "TF-Demo" {
   ami           = "ami-09d95fab7fff3776c"
   instance_type = "t2.micro"
-  user_data     = <<-EOF
+  user_data     = 
                   #!/bin/bash
                   sudo su
                   yum -y install httpd
                   echo "<p> TF-Demo! </p>" >> /var/www/html/index.html
                   sudo systemctl enable httpd
                   sudo systemctl start httpd
-                  EOF
+                  
 }
 
 output "DNS" {
